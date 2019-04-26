4120fce561b0672a5777eda4cbcaaac15f9a06ab
roman-jordan <rjordan1352@gmail.com>
2019-04-26 12:54:11 -0700

    Eye color to Avatars updating

--- starwars/src/components/StarWarsChar.js
+++ starwars/src/components/StarWarsChar.js
@@ -1,19 +1,15 @@
 import React, { Component } from 'react';
+import ScgRender from './svgRender/SvgRender';
+import SvgRender from './svgRender/SvgRender';
 
 export default function StarWarsChar(props){
-//console.log(props.char)
 
-const getAvatar = char => {
-  let sex = char.gender === 'n/a' ? 'male':char.gender;
-  let Avatar =  <object width="150px" type="image/svg+xml" data={`img/${sex}Avatar.svg`}></object>
-   return Avatar
-}
 
 return(
     <div className="StarWarsChar">
         <h2>{props.char.name}</h2>
         <p>Hello, I am {props.char.name}.</p>
-        {getAvatar(props.char)}
+        <SvgRender char={props.char}/>
 
     </div>
 )
